0d15b8dd064951a54b59b175545f3306943518b3
wawrzaszek <szymonmosor@outlook.com>
2026-09-04 08:43:38 +0200

    podlaczenie z db

--- 1/index.php
+++ 1/index.php
@@ -1,12 +1,10 @@
 <!DOCTYPE html>
 <html lang="pl">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Wyszukiwarka</title>
 </head>
-
 <body>
     <h1>Wyszukiwarka</h1>
     <?php
@@ -20,6 +18,14 @@
         <input type="text" name="query" id="query" placeholder="Wpisz frazę do wyszukiwania" value="<?= htmlspecialchars($q ?? '') ?>">
         <button type="submit">Szukaj</button>
     </form>
-</body>
 
+    <?php
+        $db = mysqli_connect('localhost', 'root', '', '5b');
+        if ($db){
+            die("Bład połączenia z bd");
+        }
+
+        mysqli_close($db);
+    ?>
+</body>
 </html>
