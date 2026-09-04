2de3a9a3ae3accfa949ce7efd57f784b7da140b4
wawrzaszek <szymonmosor@outlook.com>
2026-09-04 08:28:50 +0200

    pti

---
+++ 1/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wyszukiwarka</title>
+</head>
+
+<body>
+    <h1>Wyszukiwarka</h1>
+    <?php
+    $q = $_GET['query'] ?? null;
+    if ($q) {
+        echo "<h2>Szukasz: " . htmlspecialchars($q) . "</h2>";
+    }
+    ?>
+
+    <form action="index.php" method="get">
+        <input type="text" name="query" id="query" placeholder="Wpisz frazę do wyszukiwania" value="<?= htmlspecialchars($q ?? '') ?>">
+        <button type="submit">Szukaj</button>
+    </form>
+</body>
+
+</html>
