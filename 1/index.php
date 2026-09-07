23bd316b820c916af089aa92c49ea2ee4e122288
smosor <szymonmosor@outlook.com>
2026-09-07 10:22:40 +0200

    styl

--- 1/index.php
+++ 1/index.php
@@ -4,9 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Wyszukiwarka</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <h1>Wyszukiwarka</h1>
+
     <?php
     $q = $_GET['query'] ?? null;
     if ($q) {
@@ -20,20 +22,21 @@
     </form>
 
     <?php
+    if ($q) {
         $db = mysqli_connect('localhost', 'root', '', '5b1');
-        if (!$db){
-            die("Bład połączenia z bd");
+        if (!$db) {
+            die("Błąd połączenia z bd");
         }
 
-        if($q){
-            $q_escaped = mysqli_real_escape_string($db, $q);
-            $odp = mysqli_query($db, "SELECT * FROM dane WHERE query LIKE '%$q_escaped%'");
-            while($dane = mysqli_fetch_array($odp)){
-                echo "<div>" . htmlspecialchars($dane['query']) . "</div>";
-            }
+        $q_escaped = mysqli_real_escape_string($db, $q);
+        $odp = mysqli_query($db, "SELECT * FROM dane WHERE query LIKE '%$q_escaped%'");
+
+        while ($dane = mysqli_fetch_array($odp)) {
+            echo "<div class='wynik'>" . htmlspecialchars($dane['query']) . "</div>";
         }
 
         mysqli_close($db);
+    }
     ?>
 </body>
 </html>
